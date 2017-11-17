6349feea5d65dd11334bd7048548bca361ee1ad7
romainjpf <romainjpf.forestier@gmail.com>
2017-11-17 15:08:09 +0100

    ajouts devices

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ module.exports = function(sails) {
 
     var exec = require('./lib/exec.js');
     var install = require('./lib/install.js');
+    var uninstall = require('./lib/uninstall.js');
     var initialize = require('./lib/initialize.js');
 
     return {
         install: install,
+        uninstall: uninstall,
         initialize: initialize,
         exec: exec
     };
